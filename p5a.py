9726fd849069db416e0b007127cb05e54025a99d
肇小狼 <qz2338@columbia.edu>
2019-02-24 18:33:22 -0500

    Jacobian transpose finished

--- p5a.py
+++ p5a.py
@@ -8,6 +8,7 @@ import os
 #DH = [a, alpha, d]
 
 PI = 3.141592657
+BIAS = 1e-2
 
 def computeHomo(joint, DH):
 	temp = np.zeros((4, 4))
@@ -61,8 +62,8 @@ def computeAllHomo(joints, DH):
 
 	return Zs, Ps
 
-def computeJacobian(Zs, Ps):
-
+def computeJacobian(joints, DH):
+	Zs, Ps = computeAllHomo(joints, DH)
 	J = np.zeros((6, 7))
 
 	for i in np.arange(0, 7):
@@ -75,10 +76,79 @@ def computeJacobian(Zs, Ps):
 		J[4, i] = Zs[i][1][0]
 		J[5, i] = Zs[i][2][0]
 
-		print(Zs[i])
-		print(J)
+		# print(Zs[i])
+		# print(J)
+
+	return J
 
 
+def computeInitError(Ti, Td):
+	e = np.zeros((6, 1))
+	pi = Ti[:3, [3]]
+	pd = Td[:3, [3]]
+
+	e[0, 0] = pd[0][0] - pi[0][0]
+	e[1, 0] = pd[1][0] - pi[1][0]
+	e[2, 0] = pd[2][0] - pi[2][0]
+
+	xi = Ti[:3, [0]].transpose()
+	yi = Ti[:3, [1]].transpose()
+	zi = Ti[:3, [2]].transpose()
+
+	xd = Td[:3, [0]].transpose()
+	yd = Td[:3, [1]].transpose()
+	zd = Td[:3, [2]].transpose()
+
+	print(xd)
+	print(yd)
+	print(zd)
+
+	e0 = np.cross(xi, xd) + np.cross(yi, yd) + np.cross(zi, zd)
+
+	e[3, 0] = e0[0, 0]
+	e[4, 0] = e0[0, 1]
+	e[5, 0] = e0[0, 2]
+
+	return e
+
+
+def computeAlpha(J, e):
+	JJTe = np.matmul(np.matmul(J, J.transpose()), e).transpose()[0]
+	return np.dot(JJTe, e.transpose()[0]) / np.dot(JJTe, JJTe)
+
+
+def updateJacoTranspose(e, qi, DH):
+
+	deltax = np.matrix([[1], [1], [1], [1], [1], [1]])
+	i = 0
+	while (not checkTiny(deltax)):
+		J = computeJacobian(qi, DH)
+
+		alpha = computeAlpha(J, e)
+		print(alpha)
+
+		deltaq = np.matmul(alpha * J.transpose(), e)
+		deltax = np.matmul(J, deltaq)
+		print(deltaq)
+
+		qi += deltaq.transpose()[0]
+		e = deltax
+
+	print("all done!")
+
+
+def checkTiny(x):
+	for i in np.arange(0, 7):
+		if x[i, 0] > BIAS:
+			return False
+	return True
+
+
+def initTs():
+	Ti = np.matrix([[0.515, 0.481, 0.709, -0.0815], [0.476, -0.849, 0.230, -0.0409], [0.713, 0.219, -0.667, 0.399], [0, 0, 0, 1]])
+	Td = np.matrix([[-0.781, -0.474, 0.407, -0.0206], [-0.220, 0.818, 0.531, -0.147], [-0.585, 0.325, -0.743, 0.620], [0, 0, 0, 1]])
+
+	return Ti, Td
 
 
 def initDH(): 
@@ -99,7 +169,20 @@ def initJoints():
 DH = initDH()
 joints = initJoints()
 Zs, Ps = computeAllHomo(joints, DH)
-computeJacobian(Zs, Ps)
+J = computeJacobian(joints, DH)
+
+# P5a
+print(J)
+
+Ti, Td = initTs()
+e = computeInitError(Ti, Td)
+
+print(e)
+
+updateJacoTranspose(e, joints, DH)
+
+
+
 
 
 
